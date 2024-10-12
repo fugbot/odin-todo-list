d788073e6fdb2654fe4d3015aa033a23293056f7
fugbot <18075133+fugbot@users.noreply.github.com>
2024-10-12 09:50:45 +0000

    Fix task class order of variables

--- src/TaskComponent.js
+++ src/TaskComponent.js
@@ -1,8 +1,8 @@
 class Task {
-    constructor (title, description = "", dueDate = null, priority = "normal", completed = false){
+    constructor (title, dueDate = null, description = "", priority = "normal", completed = false){
         this.title = title;
-        this.description = description;
         this.dueDate = dueDate;
+        this.description = description;
         this.priority = priority;
         this.completed = completed;
     }
