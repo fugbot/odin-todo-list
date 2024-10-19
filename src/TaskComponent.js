72f29c0c95ebe6dd3fbf083a0dd33fffe08615c9
fugbot <18075133+fugbot@users.noreply.github.com>
2024-10-19 14:28:01 +0000

    Change Task to factory function

--- src/TaskComponent.js
+++ src/TaskComponent.js
@@ -1,19 +1,69 @@
-class Task {
-    constructor (title, dueDate = null, description = "", priority = "low", completed = false){
-        this.title = title;
-        this.dueDate = dueDate;
-        this.description = description;
-        this.priority = priority;
-        this.completed = completed;
+const Task = () => {
+  let taskCounter = 0;
+
+  return function createTask(
+    title,
+    priority,
+    dueDate = null,
+    description = "",
+    completed = false,
+    projectId
+  ) {
+    if (!title) {
+      throw new Error("Title is required");
     }
 
-    toggleComplete(){
-        this.completed = !this.completed;
-    }
-    
-    updatePriority(newPriority){
-        this.priority = newPriority;
-    }
-}
+    taskCounter++;
 
-export {Task}
+    return {
+      taskId: taskCounter,
+      title,
+      priority,
+      dueDate,
+      description,
+      completed,
+      projectId,
+
+      markAsCompleted() {
+        this.completed = true;
+      },
+
+      updateDueDate(newDate) {
+        this.dueDate = newDate;
+      },
+    };
+  };
+};
+
+// function Task() => {
+//   title,
+//   dueDate = null,
+//   priority = "low",
+//   description = "",
+//   completed = false
+//   //add project
+//   //add incrementing taskid
+// ) {
+//   if (!title) {
+//     throw new Error("Title is a required fields");
+//   }
+
+//   return {
+//     title,
+//     dueDate,
+//     priority,
+//     description,
+//     completed,
+
+//     // You can also add methods here if needed
+//     markAsCompleted() {
+//       this.completed = true;
+//     },
+
+//     updateDueDate(newDate) {
+//       this.dueDate = newDate;
+//     },
+//   };
+// }
+
+export { Task };
